adf75b7c5b4b5b0938c43e8bd69f526fe77c7cda
Egorpalan <Egorpalan@yandex.ru>
2023-11-14 22:31:26 +0400

    add redirect from archive

--- women/views.py
+++ women/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseNotFound, Http404
+from django.urls import reverse
 
 
 def index(request):
@@ -16,7 +17,8 @@ def categories_by_slug(request, cat_slug):
 
 def archive(request, year):
     if year > 2023:
-        raise Http404()
+        uri = reverse("cats", args=("music",))
+        return redirect(uri)
     return HttpResponse(f"<h1>Архив по годам</h1><p>Год: {year}</p>")
 
 
