7dc333a561ae838b42489c1b23709c01a1cfef8b
Egorpalan <Egorpalan@yandex.ru>
2023-11-25 17:00:44 +0400

    add tags and categories

--- women/models.py
+++ women/models.py
@@ -20,6 +20,15 @@ class Women(models.Model):
     time_create = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(choices=Status.choices, default=Status.DRAFT)
+    cat = models.ForeignKey("Category", on_delete=models.PROTECT, related_name="posts")
+    tags = models.ManyToManyField("TagPost", blank="True", related_name="tags")
+    husband = models.OneToOneField(
+        "Husband",
+        related_name="woman",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+    )
 
     objects = models.Manager()
     published = PublishedManager()
@@ -33,3 +42,33 @@ class Women(models.Model):
 
     def get_absolute_url(self):
         return reverse("post", kwargs={"post_slug": self.slug})
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, db_index=True)
+    slug = models.SlugField(max_length=255, unique=True, db_index=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("category", kwargs={"cat_slug": self.slug})
+
+
+class TagPost(models.Model):
+    tag = models.CharField(max_length=100, db_index=True)
+    slug = models.SlugField(max_length=255, unique=True, db_index=True)
+
+    def __str__(self):
+        return self.tag
+
+    def get_absolute_url(self):
+        return reverse("tag", kwargs={"tag_slug": self.slug})
+
+
+class Husband(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField(null=True)
+
+    def __str__(self):
+        return self.name
